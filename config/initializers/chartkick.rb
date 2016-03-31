1def8ee660fbaea9a74d60ef5bf0f9ec788badf5
FORSBERG+two <bjorn@forsbergplustwo.com>
2016-03-31 10:53:44 +0200

    Adding more colors to charts to stop colors repeating

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,6 +1,6 @@
 Chartkick.options = {
   height: "230px",
-  colors: ["#7AB55C", "#EBB129", "#368AA9"],
+  colors: ["#7AB55C", "#EBB129", "#368AA9", "#A9368A", "#E86A6A", "#30373B"],
   library: {vAxis: {textStyle:{color: '#798C9C',fontSize: '14', paddingRight: '100',marginRight: '100'}},
     hAxis: {textStyle:{color: '#798C9C',fontSize: '14', paddingRight: '100', marginRight: '100'}},
     chartArea: {width: '92%', height: '80%', left: '7%'}}
