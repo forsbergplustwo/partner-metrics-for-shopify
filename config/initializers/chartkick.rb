6090aba13c986e591f8a748f460f02d86180bcc8
FORSBERG+two <bjorn@forsbergplustwo.com>
2017-07-05 12:57:31 +0200

    Update styling to be more in line with Polaris

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,7 +1,9 @@
 Chartkick.options = {
   height: "230px",
-  colors: ["#7AB55C", "#EBB129", "#368AA9", "#A9368A", "#E86A6A", "#30373B"],
-  library: {vAxis: {textStyle:{color: '#798C9C',fontSize: '14', paddingRight: '100',marginRight: '100'}},
+  colors: ["#9C6ADE", "#47C1BF", "#F49342"],
+  library: {
+    vAxis: {textStyle:{color: '#798C9C',fontSize: '14', paddingRight: '100',marginRight: '100'}},
     hAxis: {textStyle:{color: '#798C9C',fontSize: '14', paddingRight: '100', marginRight: '100'}},
-    chartArea: {width: '92%', height: '80%', left: '7%'}}
+    chartArea: {width: '92%', height: '80%', left: '7%'}
+  }
 }
